58fe01e802f3404197edb5e3fc01bc92cb5a0194
Nicolas Dupont <nicolas@akeneo.com>
2016-11-13 18:38:01 +0100

    Chapter 4, adding UI and level transition

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MovingObject {
 
@@ -8,6 +9,7 @@ public class Player : MovingObject {
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
     public int wallDamage = 1;
+    public Text foodText;
 
     private Animator animator;
     private int food;
@@ -16,6 +18,7 @@ public class Player : MovingObject {
 	{
 	    animator = GetComponent<Animator>();
 	    food = GameManager.instance.playerFoodPoints;
+	    foodText.text = "Food: "+food;
 
 	    base.Start();
 	}
@@ -48,6 +51,7 @@ public class Player : MovingObject {
     protected override void AttemptMove <T> (int xDir, int yDir)
     {
         food--;
+        foodText.text = "Food: "+food;
         base.AttemptMove<T>(xDir, yDir);
 
         RaycastHit2D hit;
@@ -62,9 +66,11 @@ public class Player : MovingObject {
             enabled = false;
         } else if (other.tag == "Food") {
             food += pointsPerFood;
+            foodText.text = "+" + pointsPerFood + " Food: "+food;
             other.gameObject.SetActive(false);
         } else if (other.tag == "Soda") {
             food += pointsPerSoda;
+            foodText.text = "+" + pointsPerSoda + " Food: "+food;
             other.gameObject.SetActive(false);
         }
     }
@@ -85,6 +91,7 @@ public class Player : MovingObject {
     {
         animator.SetTrigger("playerHit");
         food -= loss;
+        foodText.text = "-" + loss + " Food: "+food;
         CheckIfGameOver();
     }
 
